a95a2dec75ef6f790b2ba8ee5225b0d44e499b04
root <y4812@hotmail.com>
2018-10-23 00:57:02 -0400

    pull from remote

--- update.sh
+++ update.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-# run this script to push ./docs to server...
-# delete is dangerous...
+# run this script to push/pull ./docs to/from server...
+#
+# push: ./update.sh [delete]            sync remote docs by local ./docs and update database
+# pull: ./update.sh download [delete]   sync local ./docs by remote
+# !!! delete will delete files at receive side that not exist in remote side !!!
+#
 
+PORT=27173
+REMOTE=root@server
+REMOTE_ROOT=/root/MiBlog
+
+if [ "$1" = "download" ] ; then
+    if [ "$2" = "delete" ] ; then
+        rsync_delete="--delete"
+    fi 
+    #rm -rf ./docs
+    rsync -avh --progress -e 'ssh -p '$PORT'' $rsync_delete $REMOTE:$REMOTE_ROOT/docs .
+    exit
+fi
 
 if [ "$1" = "delete" ] ; then
     gentool_delete=delete
@@ -12,13 +28,8 @@ else
     rsync_delete=""
 fi
 
-PORT=27173
-REMOTE=root@server
-REMOTE_ROOT=/root/MiBlog
-
 #scp -p -r -P $PORT ./docs $REMOTE:$REMOTE_ROOT/docs
-rsync -av -e 'ssh -p '$PORT'' $rsync_delete ./docs $REMOTE:$REMOTE_ROOT
-
+rsync -avh --progress -e 'ssh -p '$PORT'' $rsync_delete ./docs $REMOTE:$REMOTE_ROOT
 
 # easy but dirty...
 ssh -p $PORT $REMOTE "cd $REMOTE_ROOT && ./gentool.py update_all $gentool_delete"
